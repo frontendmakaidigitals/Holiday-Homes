8b4726572efeaafd46d7edee20b8e24fba77ecbf
Faheem <Faheemk793@gmail.com>
2024-10-28 20:45:21 +0530

    bug fixed

--- src/data/navigation.ts
+++ src/data/navigation.ts
@@ -59,6 +59,107 @@ const megaMenuDemo: MegamenuItem[] = [
 		})),
 	},
 ]
+const demoChildMenus: NavItemType[] = [
+	{
+		id: ncNanoId(),
+		href: '/',
+		name: 'Online booking',
+	},
+	{
+		id: ncNanoId(),
+		href: '/home-2',
+		name: 'Real estate',
+		isNew: true,
+	},
+	{
+		id: ncNanoId(),
+		href: '/home-3',
+		name: 'Home 3',
+		isNew: true,
+	},
+]
+
+const otherPageChildMenus: NavItemType[] = [
+	{ id: ncNanoId(), href: '/blog', name: 'Blog page' },
+	{ id: ncNanoId(), href: '/blog/single' as Route, name: 'Blog single' },
+	{ id: ncNanoId(), href: '/about', name: 'About' },
+	{ id: ncNanoId(), href: '/contact', name: 'Contact us' },
+	{ id: ncNanoId(), href: '/login', name: 'Login' },
+	{ id: ncNanoId(), href: '/signup', name: 'Signup' },
+]
+
+const templatesChildrenMenus: NavItemType[] = [
+	{
+		id: ncNanoId(),
+		href: '/add-listing/1' as Route,
+		name: 'Add listing',
+		type: 'dropdown',
+		children: [
+			{
+				id: ncNanoId(),
+				href: '/add-listing/1' as Route,
+				name: 'Add listing 1',
+			},
+			{
+				id: ncNanoId(),
+				href: '/add-listing/2' as Route,
+				name: 'Add listing 2',
+			},
+			{
+				id: ncNanoId(),
+				href: '/add-listing/3' as Route,
+				name: 'Add listing 3',
+			},
+			{
+				id: ncNanoId(),
+				href: '/add-listing/4' as Route,
+				name: 'Add listing 4',
+			},
+			{
+				id: ncNanoId(),
+				href: '/add-listing/5' as Route,
+				name: 'Add listing 5',
+			},
+			{
+				id: ncNanoId(),
+				href: '/add-listing/6' as Route,
+				name: 'Add listing 6',
+			},
+			{
+				id: ncNanoId(),
+				href: '/add-listing/7' as Route,
+				name: 'Add listing 7',
+			},
+			{
+				id: ncNanoId(),
+				href: '/add-listing/8' as Route,
+				name: 'Add listing 8',
+			},
+			{
+				id: ncNanoId(),
+				href: '/add-listing/9' as Route,
+				name: 'Add listing 9',
+			},
+			{
+				id: ncNanoId(),
+				href: '/add-listing/10' as Route,
+				name: 'Add listing 10',
+			},
+		],
+	},
+	//
+	{ id: ncNanoId(), href: '/checkout', name: 'Checkout' },
+	{ id: ncNanoId(), href: '/pay-done', name: 'Pay done' },
+	//
+	{ id: ncNanoId(), href: '/author', name: 'Author page' },
+	{ id: ncNanoId(), href: '/account', name: 'Account page' },
+	//
+	{
+		id: ncNanoId(),
+		href: '/subscription',
+		name: 'Subscription',
+	},
+]
 
 export const NAVIGATION_DEMO: NavItemType[] = [
 	{
