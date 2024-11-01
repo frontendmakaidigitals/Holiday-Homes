92c5afc5f7e93accff0d76c9ef8475d33112b3c4
Faheem <Faheemk793@gmail.com>
2024-11-01 20:59:42 +0530

    done

--- src/app_chunks/Navbar.tsx
+++ src/app_chunks/Navbar.tsx
@@ -1,51 +1,47 @@
-"use client";
-import React from "react";
+'use client'
+import React from 'react'
 import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from "@/components/ui/popover";
-import { IoNotificationsOutline } from "react-icons/io5";
-import { motion } from "framer-motion";
+	Popover,
+	PopoverContent,
+	PopoverTrigger,
+} from '@/components/ui/popover'
+import { IoNotificationsOutline } from 'react-icons/io5'
+import { motion } from 'framer-motion'
 
 const Navbar = () => {
-  const name = "faheem";
-  return (
-    <div className="w-full flex justify-between items-start xl:mt-5 xxl:mt-10 xl:px-3 xxl:px-10">
-      <div className="w-full">
-        <p className="font-Satoshi xl:text-2xl xxl:text-4xl font-bold  ">
-          Welcome back,{" "}
-          <span className="text-[#6b3417] font-Spline capitalize">{name}!</span>
-        </p>
-        <p className="xl:text-md xxl:text-lg font-medium font-Satoshi mt-2 text-gray-500">
-          Take a look at the updated dashboard overview
-        </p>
-      </div>
+	const name = 'faheem'
+	return (
+		<div className="flex w-full items-start justify-between xl:mt-5 xl:px-3 xxl:mt-10 xxl:px-10">
+			<div className="w-full">
+				<p className="font-Satoshi font-bold xl:text-2xl xxl:text-4xl">
+					Welcome back,{' '}
+					<span className="font-Spline capitalize text-[#6b3417]">{name}!</span>
+				</p>
+				<p className="xl:text-md font-Satoshi mt-2 font-medium text-gray-500 xxl:text-lg">
+					Take a look at the updated dashboard overview
+				</p>
+			</div>
 
-      <div>
-        <Popover>
-          <PopoverTrigger>
-            <div className="relative group">
-              <motion.div
-                animate={{ scale: [0, 1] }}
-                transition={{
-                  delay: 1,
-                }}
-                className="absolute top-0 right-1 size-[.4rem] bg-[#F44336] rounded-full"
-              />
-              <div className="absolute pointer-events-none group-hover:block hidden rounded-full top-full bg-gray-200 left-1/2 -translate-x-1/2">
-                <p className="font-Satoshi font-medium px-3 py-1 ">
-                  Notifications
-                </p>
-              </div>
-              <IoNotificationsOutline className="xl:text-xl xxl:text-3xl" />
-            </div>
-          </PopoverTrigger>
-          <PopoverContent>No new notifications</PopoverContent>
-        </Popover>
-      </div>
-    </div>
-  );
-};
+			<div>
+				<Popover>
+					<PopoverTrigger>
+						<div className="group relative">
+							<motion.div
+								animate={{ scale: [0, 1] }}
+								transition={{
+									delay: 1,
+								}}
+								className="absolute right-1 top-0 size-[.4rem] rounded-full bg-[#F44336]"
+							/>
 
-export default Navbar;
+							<IoNotificationsOutline className="xl:text-xl xxl:text-3xl" />
+						</div>
+					</PopoverTrigger>
+					<PopoverContent>No new notifications</PopoverContent>
+				</Popover>
+			</div>
+		</div>
+	)
+}
+
+export default Navbar
